8fd85db6ff3befbfa465343d8988c90c323045e4
Joao Borks <joao.borks@gmail.com>
2015-09-28 11:41:19 -0300

    Add initial hostile branch support, with a prototype branch enabled. Interaction with player not supported.

---
+++ Assets/Scripts/HostileBranch.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using System.Collections;
+
+public class HostileBranch : MonoBehaviour
+{
+    // Customizeable Variables
+    [Range(0, 90f)]
+    public float maxAngle = 45f;
+    [Range(-90f, 0)]
+    public float minAngle = -45f;
+    [Range(50f, 250f)]
+    public float rotationSpeed = 125f;
+
+    private void Awake()
+    {
+        transform.rotation = Quaternion.AngleAxis(Random.Range(minAngle, maxAngle), Vector3.right);
+
+        bool random = true;
+        if (Random.value > 0.5f)
+            random = false;
+
+        StartCoroutine(Rotate(random));
+    }
+
+    private IEnumerator Rotate(bool onward)
+    {        
+        float initX = transform.eulerAngles.x;
+        print(initX);
+        Vector3 curAngles = Vector3.zero;
+
+        curAngles.x = initX > 270 ? initX - 360 : initX;
+
+        if (onward)
+        {
+            while (curAngles.x < maxAngle)
+            {
+                curAngles.x += Time.deltaTime * rotationSpeed;
+                transform.rotation = Quaternion.Euler(curAngles);
+                yield return null;
+            }
+        }
+        else
+        {
+            while (curAngles.x > minAngle)
+            {
+                curAngles.x -= Time.deltaTime * rotationSpeed;
+                transform.rotation = Quaternion.Euler(curAngles);
+                yield return null;
+            }
+        }
+
+        StartCoroutine(Rotate(!onward));
+    }
+}
